d8a28b00624f282afef3711f79d689afdccb6788
sander1 <sanderspies+plexapp@gmail.com>
2011-04-16 05:43:07 +0200

    Load thumbs through function, default to icon-default.png if thumb is missing

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,21 +2,23 @@ import re
 import datetime
 import urlparse
 
-SF_PREFIX   = '/video/schweizerfernsehen'
 SF_ROOT     = 'http://www.videoportal.sf.tv'
 SF_SHOWS    = SF_ROOT + '/sendungen'
 SF_CHANNELS = SF_ROOT + '/channels'
 SF_SEARCH   = SF_ROOT + '/suche'
 
-CACHE_INTERVAL = 3600
+ICON        = 'icon-default.png'
+ART         = 'art-default.jpg'
 
 ####################################################################################################
 def Start():
-    Plugin.AddPrefixHandler(SF_PREFIX, GetShowOverview, 'Schweizer Fernsehen', 'icon-default.png', 'art-default.jpg')
-    Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
+    Plugin.AddPrefixHandler('/video/schweizerfernsehen', GetShowOverview, 'Schweizer Fernsehen', ICON, ART)
+    Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
     MediaContainer.title1 = 'Schweizer Fernsehen'
-    MediaContainer.art = R('art-default.jpg')
-    HTTP.CacheTime = CACHE_INTERVAL
+    MediaContainer.art = R(ART)
+    MediaContainer.viewGroup = 'InfoList'
+    DirectoryItem.thumb = R(ICON)
+    HTTP.CacheTime = CACHE_1HOUR
 
 ####################################################################################################
 def UpdateCache():
@@ -25,62 +27,65 @@ def UpdateCache():
 
 ####################################################################################################
 def GetShowOverview():
-    dir = MediaContainer(viewGroup='Details')
+    dir = MediaContainer()
     xml = HTML.ElementFromURL(SF_SHOWS)
     for show in xml.xpath('//div[@class="az_row"]'):
-        key = SF_ROOT + show.find('a').get('href')
-        title = show.xpath('a[@class="sendung_name"]')[0].text
-        description = show.xpath('p[@class="az_description"]')[0].text
+        url = SF_ROOT + show.xpath('./a')[0].get('href')
+        title = show.xpath('./a[@class="sendung_name"]')[0].text
+        description = show.xpath('./p[@class="az_description"]')[0].text
         try:
-            thumb = re.sub("width=\d+", "width=200", show.xpath('a/img')[0].get('src'))
-        except: thumb = None
-        dir.Append(Function(DirectoryItem(GetEpisodeMenu, title=title, thumb=thumb, summary=description, url=key), url=key))
+            thumb = re.sub("width=\d+", "width=200", show.xpath('./a/img')[0].get('src'))
+        except:
+            thumb = None
+        dir.Append(Function(DirectoryItem(GetEpisodeMenu, title=title, summary=description, thumb=Function(GetThumb, url=thumb)), url=url))
     return dir
 
 ####################################################################################################
 def GetEpisodeMenu(sender, url):
-    dir = MediaContainer(viewGroup='Details', title2=sender.itemTitle)
+    dir = MediaContainer(title2=sender.itemTitle)
     xml = HTML.ElementFromURL(url)
     try:
         show = xml.xpath('//div[@class="act_sendung_info"]')[0]
-        video_url = show.find('a').get('href').split(';')[0]
+        video_url = SF_ROOT + show.xpath('./a')[0].get('href').split(';')[0]
 
-        title = show.xpath('div/h2/a')[0].text
+        title = show.xpath('./div/h2/a')[0].text
         summary = ""
-        for info_item in show.xpath('//ul[@class="sendung_beitraege"]/li/a'):
+        for info_item in show.xpath('.//ul[@class="sendung_beitraege"]/li/a'):
             summary = summary + info_item.text + "\n"
         try:
-            thumb = re.sub("width=\d+", "width=200", show.xpath('a/img')[0].get('src'))
-        except: thumb = None
-        dir.Append(WebVideoItem(SF_ROOT + video_url, title=title, thumb=thumb, summary=summary))
+            thumb = re.sub("width=\d+", "width=200", show.xpath('./a/img')[0].get('src'))
+        except:
+            thumb = None
+        dir.Append(WebVideoItem(video_url, title=title, summary=summary, thumb=Function(GetThumb, url=thumb)))
     except:
         pass
 
     dir.Extend(GetPreviousEpisodes(sender, url, sender.itemTitle, previousEpisode=(len(dir) > 0)))
 
-    if (len(dir) == 0):
-        dir.Append(DirectoryItem("none", L("No Episodes")))
-
-    return dir
+    if len(dir) == 0:
+        return MessageContainer(L("No Episodes"), L("No Episodes"))
+    else:
+        return dir
 
 ####################################################################################################
 def GetPreviousEpisodes(sender, url, showTitle, previousEpisode=False):
-    dir = MediaContainer(viewGroup='Details', title2=showTitle)
+    dir = MediaContainer(title2=showTitle)
     xml = HTML.ElementFromURL(url)
 
     previous = xml.xpath('//div[@class="prev_sendungen"]')[0]
-    for show in previous.xpath('//div[@class="comment_row"]'):
+    for show in previous.xpath('.//div[@class="comment_row"]'):
         try:
-            video_url = SF_ROOT + show.xpath('div[@class="left_innner_column"]/a')[0].get('href').split(';')[0]
+            video_url = SF_ROOT + show.xpath('./div[@class="left_innner_column"]/a')[0].get('href').split(';')[0]
 
-            title = show.xpath('div[@class="sendung_content"]/a/strong')[0].text
+            title = show.xpath('./div[@class="sendung_content"]/a/strong')[0].text
             summary = ""
-            for info_item in show.xpath('div[@class="sendung_content"]/ul/li/a'):
+            for info_item in show.xpath('./div[@class="sendung_content"]/ul/li/a'):
                 summary = summary + info_item.text + "\n"
             try:
                 thumb = re.sub("width=\d+", "width=200", show.xpath('div/a/img[@class="thumbnail"]')[0].get('src'))
-            except: thumb = None
-            dir.Append(WebVideoItem(video_url, title=title, thumb=thumb, summary=summary))
+            except:
+                thumb = None
+            dir.Append(WebVideoItem(video_url, title=title, summary=summary, thumb=Function(GetThumb, url=thumb)))
         except:
             pass
 
@@ -113,10 +118,10 @@ def GetPreviousEpisodes(sender, url, showTitle, previousEpisode=False):
             (year, a, month) = date.partition("-")
             year = int(year)
             month = int(month)
-    
+
         prev_month = datetime.date(year=year, month=month, day=1)
         url = SF_ROOT + prevURL
-    
+
         if previousEpisode or len(dir) > 0 or prev_month.year < 2000:
             dir.Append(Function(DirectoryItem(GetPreviousEpisodes, title=L("Episodes from ") + L(prev_month.strftime('%B')) + " " + str(prev_month.year), url=url, thumb=R('icon-previous.png')), url=url, showTitle=showTitle))
         else:
@@ -125,3 +130,11 @@ def GetPreviousEpisodes(sender, url, showTitle, previousEpisode=False):
         pass
 
     return dir
+
+####################################################################################################
+def GetThumb(url):
+    try:
+        data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
+        return DataObject(data, 'image/jpeg')
+    except:
+        return Redirect(R(ICON))
